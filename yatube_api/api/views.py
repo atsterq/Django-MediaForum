541d378e787720b8e263e662d03c9e9ea792be8b
Олег Гущин <atsterq@gmail.com>
2023-04-14 18:35:46 +0500

    added custom permission, permissions on project level

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,43 @@
-# TODO:  Напишите свой вариант
+from api.serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer,
+)
+from api.permissions import OwnerOrReadOnly
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Follow, Group, Post
+from rest_framework import permissions, viewsets
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (OwnerOrReadOnly,)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (OwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=get_object_or_404(Post, id=self.kwargs.get("post_id")),
+        )
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return Comment.objects.filter(post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
@@ -33,7 +33,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
