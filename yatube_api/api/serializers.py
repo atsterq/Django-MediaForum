53c862c76aeb46f6fa8b19e54f09c63f96a8f346
Олег Гущин <atsterq@gmail.com>
2023-04-14 16:18:16 +0500

    added follow, group models, auth through jwt token, changed settings

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,33 @@
+from posts.models import Comment, Follow, Group, Post
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Follow
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True, slug_field="username"
     )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Comment
