83d3431c7b7f06d96721684ebcc0a718918038b3
Олег Гущин <atsterq@gmail.com>
2023-04-15 20:14:22 +0500

    fixed another bug in permission

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -9,4 +9,6 @@ class AuthorOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.author == request.user
